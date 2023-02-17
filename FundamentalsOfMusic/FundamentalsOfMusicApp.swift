6274bda65bcf594f4de68fe13763829bc3550d99
Jeremy Fitzpatrick <germsrosolino@aol.com>
2023-02-16 21:39:29 -0600

    Initial Commit

---
+++ FundamentalsOfMusic/FundamentalsOfMusicApp.swift
@@ -0,0 +1,20 @@
+//
+//  FundamentalsOfMusicApp.swift
+//  FundamentalsOfMusic
+//
+//  Created by Jeremy Fitzpatrick on 2/16/23.
+//
+
+import SwiftUI
+
+@main
+struct FundamentalsOfMusicApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
